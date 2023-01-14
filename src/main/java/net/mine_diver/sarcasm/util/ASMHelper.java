06c0502df6c6cd4d548978caca42604ab16e7d50
mineLdiver <aabesedin@mail.ru>
2023-01-14 05:37:25 +0500

    Some utility methods

--- src/main/java/net/mine_diver/sarcasm/util/ASMHelper.java
+++ src/main/java/net/mine_diver/sarcasm/util/ASMHelper.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
 
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 
 public final class ASMHelper {
@@ -24,4 +27,16 @@ public final class ASMHelper {
 	public static String toTarget(MethodNode method) {
 		return method.name + method.desc;
 	}
+
+    public static String toTarget(Class<?> owner, MethodNode methodNode) {
+        return Type.getDescriptor(owner) + methodNode.name + methodNode.desc;
+    }
+
+    public static String toTarget(ClassNode classNode, MethodNode methodNode) {
+        return "L" + classNode.name + ";" + methodNode.name + methodNode.desc;
+    }
+
+    public static String toTarget(MethodInsnNode methodInsn) {
+        return "L" + methodInsn.owner + ";" + methodInsn.name + methodInsn.desc;
+    }
 }
